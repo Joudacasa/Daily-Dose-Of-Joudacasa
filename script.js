3f4588cb413b7f52154f518b26f01408d7c1c598
Joudacasa <joudacasagames@gmail.com>
2026-03-06 14:17:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function searchVideos() {
+  const query = document.getElementById("searchBox").value.toLowerCase();
+  const results = document.getElementById("results");
+  results.innerHTML = "";
+
+  const videos = [
+    {title: "Extreme Let Go", id: "R00xb1wSUts"},
+    {title: "Deadlocked Invisible and fast", id: "g0Quj2N3nYc"},
+    {title: "Deadlocked fast", id: "hBnF4ZaEwSI"}
+  ];
+
+  const filtered = videos.filter(v => v.title.toLowerCase().includes(query));
+
+  if (filtered.length > 0) {
+    filtered.forEach(v => {
+     
+      results.innerHTML += `<iframe src="https://www.youtube.com/embed/${v.id}" allowfullscreen></iframe>`;
+    });
+  } else {
+    results.innerHTML = "<p style='margin-top: 20px;'>No videos found.</p>";
+  }
+  
+}
+
